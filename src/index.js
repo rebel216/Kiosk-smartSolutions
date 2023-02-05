791710c65568bcee7242949cf30d63493a3ecb21
rebel216 <neerajbhardwajpkt@gmail.com>
2023-02-06 02:44:40 +0530

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
