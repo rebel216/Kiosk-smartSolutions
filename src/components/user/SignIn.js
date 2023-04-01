feee7f459f9e9461e28973b7b17f0604978e9d89
rebel216 <neerajbhardwajpkt@gmail.com>
2023-04-02 01:49:25 +0530

    backend support

--- src/components/user/SignIn.js
+++ src/components/user/SignIn.js
@@ -1,82 +1,96 @@
 import React from "react";
-import { useEffect ,useState} from "react";
-import { firebase, auth } from '../../firebase';
-import { useNavigate } from 'react-router-dom';
-
-
-
+import { useEffect, useState } from "react";
+import { firebase, auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-	const [mynumber, setnumber] = useState("");
-    const [otp, setotp] = useState('');
-    const [show, setshow] = useState(false);
-    const [final, setfinal] = useState('');
-	const navigate = useNavigate();
-    // Sent OTP
-	const signin = () => {
+  const [mynumber, setnumber] = useState("");
+  const [otp, setotp] = useState("");
+  const [show, setshow] = useState(false);
+  const [final, setfinal] = useState("");
+  const navigate = useNavigate();
+  // Sent OTP
+  const signin = () => {
+    sessionStorage.clear();
 
-        sessionStorage.clear();
+    if (mynumber === "" || mynumber.length < 10) return;
 
-        if (mynumber === "" || mynumber.length < 10) return;
+    let verify = new firebase.auth.RecaptchaVerifier("recaptcha-container");
+    auth
+      .signInWithPhoneNumber("+91" + mynumber, verify)
+      .then((result) => {
+        setfinal(result);
 
-        let verify = new firebase.auth.RecaptchaVerifier('recaptcha-container');
-        auth.signInWithPhoneNumber("+91"+mynumber, verify).then((result) => {
-            setfinal(result);
+        setshow(true);
+      })
+      .catch((err) => {
+        alert(err);
+        window.location.reload();
+      });
+  };
 
-            setshow(true);
-        })
-            .catch((err) => {
-                alert(err);
-                window.location.reload()
-            });
-    }
+  // Validate OTP
+  const ValidateOtp = () => {
+    if (otp === null || final === null) return;
+    final
+      .confirm(otp)
+      .then((result) => {
+        sessionStorage.phone = JSON.stringify(mynumber);
+        console.log(mynumber);
 
-    // Validate OTP
-    const ValidateOtp = () => {
-		if (otp === null || final === null)
+        navigate("/main"); // success
+      })
+      .catch((err) => {
+        alert("Wrong code");
+      });
+  };
 
-            return;
-		final.confirm(otp).then((result) => {
-			sessionStorage.phone = JSON.stringify(mynumber);
-			console.log(mynumber)
+  const logout = () => {
+    auth.signOut();
+  };
 
-			navigate("/main") // success
-        }).catch((err) => {
-            alert("Wrong code");
-        })
-	}
-
-	const logout = () => {
-        auth.signOut();
-	}
-
-
-
-
-	return (
-		<div className="Auth-container">
-            <h1>Log in</h1>
-            
-                    <center>
-                        <div className="input-box" style={{ display: !show ? "block" : "none" }}>
-                            <input  value={mynumber} onChange={(e) => {
-                            setnumber(e.target.value) }}
-                                placeholder="phone number" />
-                            <br /><br />
-                            <div id="recaptcha-container"></div>
-                            <button className="link" onClick={signin}>SendOTP</button>
-                        </div>
-                        <div  style={{ display: show ? "block" : "none" }}>
-                            <input type="text"  placeholder={"Enter your OTP"}
-                                onChange={(e) => { setotp(e.target.value) }}></input>
-                            <br /><br />
-                            <button className='link' onClick={ValidateOtp}>Verify</button>
-                        </div>
-                    </center>
+    return (
+        <>
+      <div className="login"><h1>Log in</h1></div>
+    <div className="Auth-container ">
+      
+      <center className="">
+        <div
+          className="input-box"
+          style={{ display: !show ? "block" : "none" }}
+        >
+          <input
+            className="form-control"
+            value={mynumber}
+            onChange={(e) => {
+              setnumber(e.target.value);
+            }}
+            placeholder="phone number"
+          />
+          <br />
+          <br />
+          <div id="recaptcha-container"></div>
+          <button className="link" onClick={signin}>
+            SendOTP
+          </button>
         </div>
-
-	);
-
-}
+        <div style={{ display: show ? "block" : "none" }}>
+          <input
+            type="text"
+            placeholder={"Enter your OTP"}
+            onChange={(e) => {
+              setotp(e.target.value);
+            }}
+          ></input>
+          <br />
+          <br />
+          <button className="link" onClick={ValidateOtp}>
+            Verify
+          </button>
+        </div>
+      </center>
+    </div></>
+  );
+};
 
 export default SignIn;
